6e593d9d71c591ea862e4451858d13b9427382b8
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-19 15:57:09 +0100

    sketch main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.main')
 
 @section('main-content')
-<ul class="card-list">
-    <li class="card">
-        Comic
-    </li>
-    <li class="card">
-        Comic
-    </li>
-    <li class="card">
-        Comic
-    </li>
-</ul>
+    <ul class="card-list container">
+        @foreach ($comics as $comic)
+            <li class="card">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                <h4>
+                    {{ $comic['series'] }}
+                </h4>
+            </li>
+        @endforeach
+    </ul>
 @endsection
