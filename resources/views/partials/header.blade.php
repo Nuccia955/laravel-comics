ca16033f13eabecbc88a6e7dd10df9266a44ae2f
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-19 15:41:01 +0100

    sketch header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,17 @@
 <header>
-    <div class="logo">
-        <img src="" alt="logo">
+    <div class="container">
+        <div class="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+        </div>
+        <nav class="main-nav">
+            <ul>
+                <li>
+                    <a href="">Comics</a>
+                </li>
+                <li>
+                    <a href="">News</a>
+                </li>
+            </ul>
+        </nav>
     </div>
-    <nav class="main-nav">
-        <ul>
-            <li>
-                <a href="">Comics</a>
-            </li>
-            <li>
-                <a href="">News</a>
-            </li>
-        </ul>
-    </nav>
 </header>
