ea34c02dbf41108466760bea84416159394bd58c
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-19 16:17:33 +0100

    sketch footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,35 @@
 <footer>
-    <a href="" class="button">Sign up!</a>
-    <ul class="useful-links">
-        <li>
-            <a href="">link</a>
-        </li>
-        <li>
-            <a href="">link</a>
-        </li>
-        <li>
-            <a href="">link</a>
-        </li>
-    </ul>
+    <div class="container">
+        <a href="" class="button">Sign-up now!</a>
+        <ul class="useful-links">
+            <li>
+                <a href="">Follow us</a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('images/footer-facebook.png') }}" alt="facebook">
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('images/footer-twitter.png') }}" alt="twitter">
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('images/footer-youtube.png') }}" alt="twitter">
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('images/footer-pinterest.png') }}" alt="twitter">
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="{{ asset('images/footer-periscope.png') }}" alt="twitter">
+                </a>
+            </li>
+        </ul>
+    </div>
 </footer>
