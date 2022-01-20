77e942cb09ed783b67e6a2f0c365ef18b8d3cb61
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-20 19:56:33 +0100

    add comic-description

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -8,5 +8,38 @@
                 <img src="{{$comic['thumb'] }}" alt="{{ $comic['title'] }}">
             </div>
         </div>
+        <div class="comic">
+            <div class="container">
+                <div class="text">
+                    <h1 class="title">
+                        {{ $comic['title'] }}
+                    </h1>
+                    <div class="wrapper">
+                        <div class="details">
+                            <span>
+                                U.S. price: <strong>{{ $comic['price'] }}</strong>
+                            </span>
+                            <span>
+                                AVAILABLE
+                            </span>
+                        </div>
+                        <div class="menu-check">
+                            Check Availability
+                            <i class="fas fa-caret-down"></i>
+                        </div>
+                    </div>
+                    <div class="description">
+                        <p>
+                            {{ $comic['description']}}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="adv">
+                    <span>ADVERTISEMENT</span>
+                    <img src="{{ asset('images/adv.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
